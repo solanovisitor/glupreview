64e330e9a6b10aacfa6ed736709680f592a721f2
Solano Todeschini <solano@visitorhealth.com.br>
2021-10-01 14:57:16 -0300

    Reintroducing scanview

--- Advanced SwiftUI/ScanView.swift
+++ Advanced SwiftUI/ScanView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct ScanView: View {
+    @State private var buttonToggle = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack {
+            Image("background-1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            Color("secondaryBackground")
+                .edgesIgnoringSafeArea(.all)
+                .opacity(0.5)
+            VStack {
+                VStack(alignment: .leading, spacing: 48) {
+                    Text("Here goes the chart view!")
+                        .font(Font.largeTitle.bold())
+                        .foregroundColor(.white)
+                    ScanButton(buttonTitle: "Scan!")
+            }
+                .padding(20)
+        }
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color.white.opacity(0.2))
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .background(VisualEffectBlur(blurStyle: .systemThinMaterialDark))
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30))
+            .cornerRadius(30)
+            .padding(.horizontal)
+        }
+    
 }
 
 struct ScanView_Previews: PreviewProvider {
     static var previews: some View {
         ScanView()
+        }
     }
 }
+
